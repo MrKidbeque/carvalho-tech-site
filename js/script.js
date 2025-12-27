48760f507c0a609a2c1738f280bff06fdcd5851c
MrKidbeque <91539218+MrKidbeque@users.noreply.github.com>
2025-12-27 09:10:40 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-    const btnMenu = document.getElementById('btnMenu');
-    const menu = document.getElementById('menuOpcoes');
-
-    btnMenu.addEventListener('click', () => {
-        menu.classList.toggle('ativo');
-    });
